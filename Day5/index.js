783b2fa99d943a634a7fc70c11bb649a20673840
The Bhai Code <codethebhai@gmail.com>
2026-02-26 23:17:55 +0530

    Added Day5 Code

---
+++ Day5/index.js
@@ -0,0 +1,26 @@
+// function handleClick(){
+//     console.log("Key Press");
+
+// }
+
+// const input = document.getElementById("name")
+// console.log(input);
+
+// function handleInput(e) {
+//     console.log(e.data);
+    
+// }
+
+// input.addEventListener("input", handleInput)
+
+
+// let day5 = document.getElementById("day5")
+
+// day5.innerText = "Day 6"
+// // day5.style.fontSize = "10px"
+
+// let day5class = document.querySelectorAll(".day5")
+
+// day5class.length
+
+// console.log(day5class.length);
